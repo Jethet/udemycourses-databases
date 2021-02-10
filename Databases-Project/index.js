eea4214e4b56396701ca421291bb3bdaa78a8bc7
Jethet <henriette.hettinga@protonmail.com>
2021-02-10 18:34:24 +0100

    Add post and get queries for attendance

--- Databases-Project/index.js
+++ Databases-Project/index.js
@@ -14,6 +14,33 @@ const pool = new Pool({
   port: 5432,
 });
 
+// LANGUAGES
+app.get("/languages", function (req, res) {
+  pool.query("SELECT * FROM languages", (error, result) => {
+    res.json(result.rows);
+  });
+});
+
+app.post("/languages", (req, res) => {
+  const data = {
+    name: req.body.name,
+  };
+
+  pool
+    .query("INSERT INTO languages (name) VALUES ($1);", [data.name])
+    .then(() => res.send(`Language ${data.name} has been added.`))
+    .catch((e) => console.error(e));
+});
+
+app.delete("/languages/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool
+    .query("DELETE FROM languages WHERE id = $1;", [id])
+    .then(() => res.send(`Language with id ${id} successfully deleted.`))
+    .catch((e) => console.error(e));
+});
+
 // STUDENTS
 app.get("/students", function (req, res) {
   pool.query("SELECT * FROM students", (error, result) => {
@@ -49,6 +76,15 @@ app.post("/students", (req, res) => {
     .catch((e) => console.error(e));
 });
 
+app.delete("/students/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool
+    .query("DELETE FROM students WHERE id = $1;", [id])
+    .then(() => res.send(`Student with id ${id} successfully deleted.`))
+    .catch((e) => console.error(e));
+});
+
 // TEACHERS
 app.get("/teachers", function (req, res) {
   pool.query("SELECT * FROM teachers", (error, result) => {
@@ -70,6 +106,15 @@ app.post("/teachers", (req, res) => {
       [data.name, data.email, data.country, data.language]
     )
     .then(() => res.send(`Teacher ${data.name} has been created.`))
+    .catch((e) => console.error(e));
+});
+
+app.delete("/teachers/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool
+    .query("DELETE FROM teachers WHERE id = $1;", [id])
+    .then(() => res.send(`Teacher with id ${id} successfully deleted.`))
     .catch((e) => console.error(e));
 });
 
@@ -108,27 +153,12 @@ app.post("/staff", (req, res) => {
     .catch((e) => console.error(e));
 });
 
-// CLASSES
-app.get("/classes", function (req, res) {
-  pool.query("SELECT * FROM classes", (error, result) => {
-    res.json(result.rows);
-  });
-});
-
-app.post("/classes", (req, res) => {
-  const data = {
-    datetime: req.body.datetime,
-    topic: req.body.topic,
-    teacher: req.body.teacher,
-  };
+app.delete("/staff/:id", (req, res) => {
+  const id = parseInt(req.params.id);
 
   pool
-    .query("INSERT INTO classes (datetime, topic, teacher) VALUES ($1, $2, $3);", [
-      data.datetime,
-      data.topic,
-      data.teacher,
-    ])
-    .then(() => res.send(`Class ${data.topic} has been created.`))
+    .query("DELETE FROM staff WHERE id = $1;", [id])
+    .then(() => res.send(`Staff member with id ${id} successfully deleted.`))
     .catch((e) => console.error(e));
 });
 
@@ -154,19 +184,45 @@ app.post("/modules", (req, res) => {
     .catch((e) => console.error(e));
 });
 
-// LANGUAGES
-app.get("/languages", function (req, res) {
-  pool.query("SELECT * FROM languages", (error, result) => {
+app.delete("/modules/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool
+    .query("DELETE FROM modules WHERE id = $1;", [id])
+    .then(() => res.send(`Module with id ${id} successfully deleted.`))
+    .catch((e) => console.error(e));
+});
+
+// CLASSES
+app.get("/classes", function (req, res) {
+  pool.query("SELECT * FROM classes", (error, result) => {
     res.json(result.rows);
   });
 });
 
-app.post("/languages", (req, res) => {
-  const name = req.body.name;
+app.post("/classes", (req, res) => {
+  const data = {
+    datetime: req.body.datetime,
+    topic: req.body.topic,
+    teacher: req.body.teacher,
+  };
 
   pool
-    .query("INSERT INTO languages (name) VALUES ($1);", [name])
-    .then(() => res.send(`Language ${name} has been added.`))
+    .query("INSERT INTO classes (datetime, topic, teacher) VALUES ($1, $2, $3);", [
+      data.datetime,
+      data.topic,
+      data.teacher,
+    ])
+    .then(() => res.send(`Class ${data.topic} has been created.`))
+    .catch((e) => console.error(e));
+});
+
+app.delete("/classes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool
+    .query("DELETE FROM classes WHERE id = $1;", [id])
+    .then(() => res.send(`Class with id ${id} successfully deleted.`))
     .catch((e) => console.error(e));
 });
 
@@ -178,7 +234,25 @@ app.get("/attendance", function (req, res) {
 });
 
 app.post("/attendance", (req, res) => {
-  pool.query("INSERT");
+  const student_id = req.body.student_id;
+  const class_id = req.body.class_id;
+
+  pool
+    .query("INSERT INTO attendance (student_id, class_id) VALUES ($1, $2);", [
+      student_id,
+      class_id,
+    ])
+    .then(() => res.send(`Attendance data for student ${student_id} successfully added.`))
+    .catch((e) => console.error(e));
+});
+
+app.delete("/attendance/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool
+    .query("DELETE FROM attendance WHERE id = $1;", [id])
+    .then(() => res.send(`Attendance data for id ${id} successfully deleted.`))
+    .catch((e) => console.error(e));
 });
 
 app.listen(3000, () => {
